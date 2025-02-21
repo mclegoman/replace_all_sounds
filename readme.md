61b6da00de91c78a15b3278157e3d31b0240f0cf
dannytaylor <daniel@mclegoman.com>
2025-02-21 15:47:30 +1300

    low battery... please recharge the repository and try again.

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Replace All Sounds
+Replaces all sounds with the low battery sound.
+
+## Dependencies
+- [Fabric Loader](https://fabricmc.net) or [Quilt Loader](https://quiltmc.org)
+
+## How to change the sound
+Create a resource pack and replace `replace_all_sounds:sounds/replace_all_sounds.ogg` with your sound. By default, this is set to the low battery sound...
+
+##  
+Licensed under LGPL-3.0-or-later.
+
+**This mod is not affiliated with/endorsed by Mojang Studios, or Microsoft.**  
+The Minecraft Logo, and name are property of Mojang Studios and fall under Minecraft EULA.
---
+++ src/main/java/com/mclegoman/replace_all_sounds/mixin/SoundMixin.java
@@ -0,0 +1,21 @@
+package com.mclegoman.replace_all_sounds.mixin;
+
+import net.minecraft.client.sound.Sound;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.floatprovider.FloatSupplier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Sound.class)
+public abstract class SoundMixin {
+	@Mutable @Shadow @Final private Identifier id;
+	@Inject(at = @At(value = "RETURN"), method = "<init>")
+	private void replace_all_sounds$replaceSound(Identifier id, FloatSupplier volume, FloatSupplier pitch, int weight, Sound.RegistrationType registrationType, boolean stream, boolean preload, int attenuation, CallbackInfo ci) {
+		this.id = Identifier.of("replace_all_sounds", "replace_all_sounds");
+	}
+}
