9311ebb841e1ecffab48115c8052e527545929b9
dannytaylor <daniel@mclegoman.com>
2025-02-24 03:46:56 +1300

    entries can now have multiple inputs (outputs must still be singular, as they are set when resources are reloaded).

--- src/main/java/com/mclegoman/replace_all_sounds/ReplaceSoundManager.java
+++ src/main/java/com/mclegoman/replace_all_sounds/ReplaceSoundManager.java
@@ -10,22 +10,15 @@ import net.minecraft.util.JsonHelper;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ReplaceSoundManager {
 	private static final Map<Identifier, Identifier> registry = new HashMap<>();
 	public static Identifier getSound(Identifier identifier) {
 		if (!registry.isEmpty()) {
-			if (registry.containsKey(identifier)) {
-				return registry.get(identifier);
-			}
-			else if (registry.containsKey(Identifier.of(identifier.getNamespace(), "all"))) {
-				return registry.get(Identifier.of(identifier.getNamespace(), "all"));
-			}
-			else if (registry.containsKey(Identifier.of("all", "all"))) {
-				return registry.get(Identifier.of("all", "all"));
-			}
+			if (registry.containsKey(identifier)) return registry.get(identifier);
+			else if (registry.containsKey(Identifier.of(identifier.getNamespace(), "all"))) return registry.get(Identifier.of(identifier.getNamespace(), "all"));
+			else if (registry.containsKey(Identifier.of("all", "all"))) return registry.get(Identifier.of("all", "all"));
 		}
 		return identifier;
 	}
@@ -35,7 +28,17 @@ public class ReplaceSoundManager {
 			try (InputStream stream = resource.getInputStream()) {
 				JsonElement jsonElement = JsonParser.parseReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
 				JsonObject reader = jsonElement.getAsJsonObject();
-				if (JsonHelper.getBoolean(reader, "enabled", true)) registry.put(Identifier.of(JsonHelper.getString(reader, "input")), Identifier.of(JsonHelper.getString(reader, "output")));
+				List<Identifier> inputs = new ArrayList<>();
+				if (JsonHelper.hasElement(reader, "input")) {
+					JsonElement input = JsonHelper.getElement(reader, "input");
+					if (input.isJsonArray()) input.getAsJsonArray().forEach((i) -> inputs.add(Identifier.of(i.getAsString())));
+					else inputs.add(Identifier.of(input.getAsString()));
+				}
+				if (JsonHelper.getBoolean(reader, "enabled", true)) {
+					for (Identifier input : inputs) {
+						if (JsonHelper.hasString(reader, "output")) registry.put(input, Identifier.of(JsonHelper.getString(reader, "output")));
+					}
+				}
 				else registry.remove(Identifier.of(JsonHelper.getString(reader, "input")));
 			} catch (Exception error) {
 				System.out.println(error.getLocalizedMessage());
